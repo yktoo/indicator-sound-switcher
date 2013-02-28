d8d6ec4590c30eb6878bca10a4706a188d2bee95
Dmitry Kann <yktooo@gmail.com>
2013-02-28 23:26:59 +0100

    Prepared files for Distutils and packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='indicator-sound-switcher',
+    version='1.0.0',
+    description='Sound input/output selector indicator',
+    author='Dmitry Kann',
+    author_email='yktooo@gmail.com',
+    url='https://github.com/yktoo/indicator-sound-switcher',
+    license='GPL3',
+    package_dir={'': 'lib'},
+    py_modules=['lib_pulseaudio'],
+    scripts=['indicator-sound-switcher'],
+    data_files=[
+        ('/usr/share/applications',                      ['indicator-sound-switcher.desktop']),
+        ('/usr/share/icons/ubuntu-mono-dark/status/22',  ['icons/ubuntu-mono-dark/indicator_sound_switcher.svg']),
+        ('/usr/share/icons/ubuntu-mono-light/status/22', ['icons/ubuntu-mono-light/indicator_sound_switcher.svg']),
+    ]
+)
