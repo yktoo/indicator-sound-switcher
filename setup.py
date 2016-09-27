bbdb0a421f48fb22fc67d132f9991d734c638a4c
Dmitry Kann <yktooo@gmail.com>
2016-09-27 21:50:08 +0200

    Initial support for localisation #28

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from distutils.core import setup, Command
+import os
+
+package_name = 'indicator-sound-switcher'
+
+
+def generate_translation_files():
+    """Returns the list of .mo translation data files."""
+    lang_files = [
+        (
+            'share/locale/{}/LC_MESSAGES'.format(lang),
+            [os.path.join('locale', lang, 'LC_MESSAGES', package_name + '.mo')]
+        ) for lang in os.listdir('locale')
+    ]
+
+    return lang_files
+
 
 setup(
-    name='indicator-sound-switcher',
-    version='2.0.1ubuntu0',
+    name=package_name,
+    version='2.0.2ubuntu0',
     description='Sound input/output selector indicator',
     author='Dmitry Kann',
     author_email='yktooo@gmail.com',
@@ -14,10 +30,10 @@ setup(
     packages=['indicator_sound_switcher'],
     scripts=['indicator-sound-switcher'],
     data_files=[
-        ('/usr/share/applications',                      ['indicator-sound-switcher.desktop']),
-        ('/etc/xdg/autostart',                           ['indicator-sound-switcher.desktop']),
-        ('/usr/share/icons/ubuntu-mono-dark/status/22',  ['icons/ubuntu-mono-dark/indicator-sound-switcher.svg']),
-        ('/usr/share/icons/ubuntu-mono-light/status/22', ['icons/ubuntu-mono-light/indicator-sound-switcher.svg']),
-        ('/usr/share/icons/hicolor/22x22/status',        ['icons/default/indicator-sound-switcher.svg']),
-    ]
+        ('share/applications',                      ['indicator-sound-switcher.desktop']),
+        ('/etc/xdg/autostart',                      ['indicator-sound-switcher.desktop']),
+        ('share/icons/ubuntu-mono-dark/status/22',  ['icons/ubuntu-mono-dark/indicator-sound-switcher.svg']),
+        ('share/icons/ubuntu-mono-light/status/22', ['icons/ubuntu-mono-light/indicator-sound-switcher.svg']),
+        ('share/icons/hicolor/22x22/status',        ['icons/default/indicator-sound-switcher.svg']),
+    ] + generate_translation_files(),
 )
