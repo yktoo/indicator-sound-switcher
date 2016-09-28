c98c6c2e1e1872e642f0a02359d0cb6ac4806b37
Dmitry Kann <yktooo@gmail.com>
2016-09-28 10:43:32 +0200

    Made translations work with default locale dir (#28); other fixes

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup, Command
+from distutils.core import setup
 import os
 
-package_name = 'indicator-sound-switcher'
 
+data_files = [
+    # App shortcut
+    ('share/applications',                      ['indicator-sound-switcher.desktop']),
 
-def generate_translation_files():
-    """Returns the list of .mo translation data files."""
-    lang_files = [
+    # Autostart entry
+    ('/etc/xdg/autostart',                      ['indicator-sound-switcher.desktop']),
+
+    # Icons
+    ('share/icons/ubuntu-mono-dark/status/22',  ['icons/ubuntu-mono-dark/indicator-sound-switcher.svg']),
+    ('share/icons/ubuntu-mono-light/status/22', ['icons/ubuntu-mono-light/indicator-sound-switcher.svg']),
+    ('share/icons/hicolor/22x22/status',        ['icons/default/indicator-sound-switcher.svg']),
+]
+
+# Add all available .mo translation files to the list data files
+for lang in os.listdir('locale'):
+    data_files.append(
         (
             'share/locale/{}/LC_MESSAGES'.format(lang),
-            [os.path.join('locale', lang, 'LC_MESSAGES', package_name + '.mo')]
-        ) for lang in os.listdir('locale')
-    ]
+            [os.path.join('locale', lang, 'LC_MESSAGES', 'indicator-sound-switcher.mo')]
+        )
+    )
 
-    return lang_files
-
-
+# Configure
 setup(
-    name=package_name,
+    name='indicator-sound-switcher',
     version='2.0.2ubuntu0',
     description='Sound input/output selector indicator',
     author='Dmitry Kann',
@@ -29,11 +38,5 @@ setup(
     package_dir={'': 'lib'},
     packages=['indicator_sound_switcher'],
     scripts=['indicator-sound-switcher'],
-    data_files=[
-        ('share/applications',                      ['indicator-sound-switcher.desktop']),
-        ('/etc/xdg/autostart',                      ['indicator-sound-switcher.desktop']),
-        ('share/icons/ubuntu-mono-dark/status/22',  ['icons/ubuntu-mono-dark/indicator-sound-switcher.svg']),
-        ('share/icons/ubuntu-mono-light/status/22', ['icons/ubuntu-mono-light/indicator-sound-switcher.svg']),
-        ('share/icons/hicolor/22x22/status',        ['icons/default/indicator-sound-switcher.svg']),
-    ] + generate_translation_files(),
+    data_files=data_files,
 )
