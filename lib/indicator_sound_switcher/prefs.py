93ace7c15c5553c76298b7215ca22e7befad4878
Dmitry Kann <yktooo@gmail.com>
2018-11-04 20:02:17 +0100

    Put device settings in every listbox row

--- lib/indicator_sound_switcher/prefs.py
+++ lib/indicator_sound_switcher/prefs.py
@@ -3,6 +3,8 @@ import logging
 
 from gi.repository import Gtk
 
+from .card import Card
+
 
 class PreferencesDialog(Gtk.Dialog):
     """Indicator preferences dialog."""
@@ -51,9 +53,6 @@ class BasePage(Gtk.Box):
             self.initialise()
             self.is_initialised = True
 
-            # Show all child widgets
-            self.show_all()
-
     @abc.abstractmethod
     def initialise(self):
         """Must initialise the page."""
@@ -92,6 +91,9 @@ class GeneralPage(BasePage):
         self.switch_inputs.set_active (self.indicator.config['show_inputs',  True])
         self.switch_outputs.set_active(self.indicator.config['show_outputs', True])
 
+        # Show all child widgets
+        self.show_all()
+
     def on_switch_inputs_set(self, widget: Gtk.Switch, data):
         """Signal handler: Show Inputs switch set."""
         if not self.is_initialised:
@@ -121,34 +123,74 @@ class DevicesPage(BasePage):
 
         # Add a list box
         self.list_box = Gtk.ListBox()
+        self.list_box.connect('row-selected', self.on_device_row_selected)
         scrollbox.add(self.list_box)
 
     def initialise(self):
         for idx, card in self.indicator.cards.items():
             # Add a grid
-            grid = Gtk.Grid(border_width=12, column_spacing=6, row_spacing=6)
-            # -- Add an icon
+            grid = Gtk.Grid(border_width=12, column_spacing=6, row_spacing=6, hexpand=True)
+
+            # Add a list box row
+            row = Gtk.ListBoxRow(child=grid)
+
+            # Add an icon
             grid.attach(Gtk.Image.new_from_icon_name('yast_soundcard', Gtk.IconSize.MENU), 0, 0, 1, 2)
-            # -- Add a device title label
+
+            # Add a device title label
             title_label = Gtk.Label(xalign=0)
             title_label.set_markup('<b>{}</b>'.format(card.get_display_name()))
             grid.attach(title_label, 1, 0,  1, 1)
-            # -- Add a device name label
-            grid.attach(Gtk.Label(card.name), 1, 1,  1, 1)
-            # -- Add a configure button
-            box = Gtk.Box(hexpand=True, vexpand=True)
-            btn = Gtk.Button(_('Configure…'))
-            btn.connect('clicked', self.on_configure_btn_clicked, idx)
-            box.pack_end(btn, False, False, 0)
-            grid.attach(box, 2, 1,  1, 2)
+
+            # Add a device name label
+            grid.attach(Gtk.Label(card.name, xalign=0), 1, 1,  1, 1)
+
+            # Add a device settings box
+            row.dev_settings_box = self._get_device_settings_box(card)
+            grid.attach(row.dev_settings_box, 0, 2,  2, 1)
 
             # Add the grid as a list row
-            self.list_box.add(Gtk.ListBoxRow(child=grid))
+            self.list_box.add(row)
 
-    def on_configure_btn_clicked(self, widget, idx: int):
-        """Signal handler: Configure Device button clicked."""
-        logging.debug('.on_configure_btn_clicked(idx=%d)', idx)
-        # TODO
+        # Show all child widgets
+        self.show_all()
+
+        # Update the visibility of device widgets
+        self.update_device_widgets()
+
+    @staticmethod
+    def _get_device_settings_box(card: Card) -> Gtk.Box:
+        """Create and return a box with device settings."""
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6, border_width=12, hexpand=True, vexpand=True)
+
+        # Add a name label/entry
+        bx_name = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        bx_name.pack_start(Gtk.Label(_('Name'), xalign=0),  False, True, 0)
+        bx_name.pack_end(Gtk.Entry(),  True, True, 0)
+        box.pack_start(bx_name, False, True, 0)
+
+        # Add a label for port list box
+        box.pack_start(Gtk.Label(_('Ports'), xalign=0),  False, True, 0)
+
+        # Add a list box with ports
+        list_box = Gtk.ListBox()
+        for name, port in card.ports.items():
+            list_box.add(Gtk.Label(port.get_display_name(), xalign=0))
+        box.pack_start(list_box, True, True, 0)
+        return box
+
+    def update_device_widgets(self):
+        """Update widgets in each device row."""
+        sel_row = self.list_box.get_selected_row()
+        for row in self.list_box.get_children():
+            if row == sel_row:
+                row.dev_settings_box.show_all()
+            else:
+                row.dev_settings_box.hide()
+
+    def on_device_row_selected(self, list_box: Gtk.ListBox, row: Gtk.ListBoxRow):
+        """Signal handler: devices list box row (un)selected."""
+        self.update_device_widgets()
 
 
 class MainNotebook(Gtk.Notebook):
