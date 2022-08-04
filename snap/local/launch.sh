d8231b66540cbff9a9b090c949a54e13f1956198
eth481642 <maximdima25@gmail.com>
2022-08-04 19:50:33 +0900

    Update launch.sh

--- snap/local/launch.sh
+++ snap/local/launch.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Make sure a link to desktop launcher is added to the autostart dir
-autostart_dir="$SNAP_USER_DATA/.config/autostart"
-if [ ! -d "$autostart_dir" ]; then
-	mkdir -p "$autostart_dir"
-	ln -sfnt "$autostart_dir/" "$SNAP/share/applications/indicator-sound-switcher.desktop"
+
+AUTOSTART="$SNAP_USER_DATA/.config/autostart"
+
+if [ ! -d "$AUTOSTART" ]; then
+    mkdir -p "$AUTOSTART"
 fi
+
+ln -sfnt "$AUTOSTART/" "$SNAP/share/applications/indicator-sound-switcher.desktop"
 
 exec "$@"
