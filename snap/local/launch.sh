c527d8a216b6ac06591391db75cc9b9fbcc09096
eth481642 <maximdima25@gmail.com>
2022-08-06 21:18:26 +0900

    Fix autostart after multiple snap updates (#127)
    * Update launch.sh
    * Update snapcraft.yaml
    * Update launch.sh
    * Update snap/local/launch.sh
    
    Co-authored-by: Dmitry Kann <yktooo@gmail.com>

--- snap/local/launch.sh
+++ snap/local/launch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Make sure a link to desktop launcher is added to the autostart dir
-autostart_dir="$SNAP_USER_DATA/.config/autostart"
-if [ ! -d "$autostart_dir" ]; then
-	mkdir -p "$autostart_dir"
-	ln -sfnt "$autostart_dir/" "$SNAP/share/applications/indicator-sound-switcher.desktop"
-fi
+
+AUTOSTART="$SNAP_USER_DATA/.config/autostart/"
+
+mkdir -p "$AUTOSTART"
+
+ln -sfnt "$AUTOSTART" "$SNAP/share/applications/indicator-sound-switcher.desktop"
 
 exec "$@"
